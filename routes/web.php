7cce55348373b91676be9d004d61b82c0b587221
Esteban Arevalo <juan.lopez@mantum.com.co>
2024-06-15 20:49:38 -0500

    Prueba de rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('prueba/{variable?}/{nombre?}/{apodo?}', function ($variable = null, $nombre = null, $apodo = null) {
+    if (!empty($variable) && !empty($nombre) && !empty($apodo)) {
+        return 'Ruta de prueba con variable: ' . $variable . ', nombre ' . $nombre . ' y apodo ' . $apodo;
+    } else if (!empty($variable) && !empty($nombre) && empty($apodo)) {
+        return 'Ruta de prueba con variable: ' . $variable . ' y nombre ' . $nombre;
+    } else if (!empty($variable) && empty($nombre) && empty($apodo)) {
+        return 'Ruta de prueba con variable: ' . $variable;
+    } else {
+        return 'Ruta de prueba sin parámetros';
+    }
+});
